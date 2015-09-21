111220fdc98b863f29ea37ef8c10a2066401f9f0
Robert Underwood <rr.underwood94@gmail.com>
2015-09-20 23:37:02 -0400

    Implemented bulk of functionality

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
+	int i;
+	
+	sscanf(argv[1], "%d", &i);
+
+	if (i % 3 == 0){
+		printf("fizz");
+	}
+	if (i % 2 == 0){
+		printf("buzz");
+	}
+	printf("\n");
 	return 0;
 }
