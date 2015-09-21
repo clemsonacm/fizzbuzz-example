184c9a1c6c78e968726bd28e48c848b9df8a2e44
Robert Underwood <rr.underwood94@gmail.com>
2015-09-20 23:38:52 -0400

    Added test to ensure proper inputs

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 	int i;
 	
 	if (argc != 2) return 1;
-	sscanf(argv[1], "%d", &i);
+	if (sscanf(argv[1], "%d", &i) != 1) return 1;
 
 	if (i % 3 == 0){
 		printf("fizz");
